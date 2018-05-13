6fa927503b79de5e212cd3b8d934862685cbbffc
EricYYY6 <marcusyyy1992@gmail.com>
2018-05-13 14:38:27 -0700

    ip address modified

--- service/main.go
+++ service/main.go
@@ -20,7 +20,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.225.12.102:9200"
+	ES_URL = "http://35.232.29.149:9200"
 )
 
 
