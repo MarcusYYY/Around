61b5565a7aa4599735b5c86beffbf0bdbcaf428a
EricYYY6 <marcusyyy1992@gmail.com>
2018-05-27 16:13:05 -0700

    Added image classifiers to the project. And we are able to get evaluation by sending http request

--- service/main.go
+++ service/main.go
@@ -5,19 +5,84 @@ import (
 	"fmt"
 	"net/http"
 	"encoding/json"
-	"log"
+	//"log"
 	"strconv"
 	"reflect"
 	"io"
+	"log"
 	"github.com/pborman/uuid"
 	"strings"
 	"context"
 	"cloud.google.com/go/storage"
-	//"github.com/auth0/go-jwt-middleware"
+	"github.com/auth0/go-jwt-middleware"
 	"github.com/dgrijalva/jwt-go"
-	//"github.com/gorilla/mux"
+	"path/filepath"
+	"github.com/gorilla/mux"
 )
 var mySigningKey = []byte("secret")
+
+func handlerCluster(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received one request for clustering")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+
+	if r.Method != "GET" {
+		return
+	}
+
+	term := r.URL.Query().Get("term")
+
+	// Create a client
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		http.Error(w, "ES is not setup", http.StatusInternalServerError)
+		fmt.Printf("ES is not setup %v\n", err)
+		return
+	}
+
+	// Range query.
+	// For details, https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-range-query.html
+	q := elastic.NewRangeQuery(term).Gte(0.9)
+
+	searchResult, err := client.Search().
+		Index(INDEX).
+		Query(q).
+		Pretty(true).
+		Do()
+	if err != nil {
+		// Handle error
+		m := fmt.Sprintf("Failed to query ES %v", err)
+		fmt.Println(m)
+		http.Error(w, m, http.StatusInternalServerError)
+	}
+
+	// searchResult is of type SearchResult and returns hits, suggestions,
+	// and all kinds of other information from Elasticsearch.
+	fmt.Printf("Query took %d milliseconds\n", searchResult.TookInMillis)
+	// TotalHits is another convenience function that works even when something goes wrong.
+	fmt.Printf("Found a total of %d post\n", searchResult.TotalHits())
+
+	// Each is a convenience function that iterates over hits in a search result.
+	// It makes sure you don't need to check for nil values in the response.
+	// However, it ignores errors in serialization.
+	var typ Post
+	var ps []Post
+	for _, item := range searchResult.Each(reflect.TypeOf(typ)) {
+		p := item.(Post)
+		ps = append(ps, p)
+
+	}
+	js, err := json.Marshal(ps)
+	if err != nil {
+		m := fmt.Sprintf("Failed to parse post object %v", err)
+		fmt.Println(m)
+		http.Error(w, m, http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(js)
+}
 
 
 const (
@@ -28,8 +93,9 @@ const (
 	PROJECT_ID = "prefab-wonder-203921"
 	BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.184.34.255:9200"
+	ES_URL = "http://35.202.72.195:9200"
 	BUCKET_NAME = "post-image-203922"
+	API_PREFIX      = "/api/v1"
 )
 
 
@@ -40,11 +106,26 @@ type Location struct {
 
 type Post struct {
   // `json:"user"` is for the json parsing of this User field. Otherwise, by default it's 'User'.
-  User     string `json:"user"`
-  Message  string  `json:"message"`
-  Location Location `json:"location"`
-  Url    string `json:"url"`
+	  User     string `json:"user"`
+	  Message  string  `json:"message"`
+	  Location Location `json:"location"`
+	  Url    string `json:"url"`
+	  Type     string   `json:"type"`
+	  Face     float64  `json:"face"`
 }
+var (
+	mediaTypes = map[string]string{
+		".jpeg": "image",
+		".jpg":  "image",
+		".gif":  "image",
+		".png":  "image",
+		".mov":  "video",
+		".mp4":  "video",
+		".avi":  "video",
+		".flv":  "video",
+		".wmv":  "video",
+	}
+)
 
 func main() {
 	// Create a client
@@ -79,13 +160,32 @@ func main() {
 			panic(err)
 		}
 	}
-
 	fmt.Println("started-service")
-	http.HandleFunc("/post", handlerPost)
-	http.HandleFunc("/search", handlerSearch)
+	r := mux.NewRouter()
+
+	var jwtMiddleware = jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			return mySigningKey, nil
+		},
+		SigningMethod: jwt.SigningMethodHS256,
+	})
+
+	r.Handle(API_PREFIX+"/post", jwtMiddleware.Handler(http.HandlerFunc(handlerPost)))
+	r.Handle(API_PREFIX+"/search", jwtMiddleware.Handler(http.HandlerFunc(handlerSearch)))
+	r.Handle(API_PREFIX+"/cluster", jwtMiddleware.Handler(http.HandlerFunc(handlerCluster)))
+
+	r.Handle(API_PREFIX+"/login", http.HandlerFunc(loginHandler))
+	r.Handle(API_PREFIX+"/signup", http.HandlerFunc(signupHandler))
+
+	// Backend endpoints.
+	http.Handle(API_PREFIX+"/", r)
+	// Frontend endpoints.
+	http.Handle("/", http.FileServer(http.Dir("build")))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
+
 }
+
 func containsFilteredWords(s *string) bool {
 	filteredWords := []string{
 		"fuck",
@@ -137,6 +237,26 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer file.Close()
+	im, header, _ := r.FormFile("image")
+	defer im.Close()
+	suffix := filepath.Ext(header.Filename)
+
+	// Client needs to know the media type so as to render it.
+	if t, ok := mediaTypes[suffix]; ok {
+		p.Type = t
+	} else {
+		p.Type = "unknown"
+	}
+	// ML Engine only supports jpeg.
+	if suffix == ".jpeg" {
+		if score, err := annotate(im); err != nil {
+			http.Error(w, "Failed to annotate the image", http.StatusInternalServerError)
+			fmt.Printf("Failed to annotate the image %v\n", err)
+			return
+		} else {
+			p.Face = score
+		}
+	}
 
 	ctx := context.Background()
 
@@ -246,6 +366,7 @@ func saveToES(p *Post, id string) {
 
 
 func handlerSearch(w http.ResponseWriter, r *http.Request) {
+
 	fmt.Println("Received one request for search.")
 	lat, _ := strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
 	lon, _ := strconv.ParseFloat(r.URL.Query().Get("lon"), 64)
